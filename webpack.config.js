09003fbcfcd98436bc2a5fbd6a4f3eda94796a38
Terrence <terrencebattle30@gmail.com>
2025-01-27 11:03:13 -0500

    AAA

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
     },
     module: {
@@ -28,7 +28,7 @@ module.exports = {
         }),
     ],
     devServer: {
-        static: './dist',
+        static: './build',
         port: 3000,
     },
     resolve: {
