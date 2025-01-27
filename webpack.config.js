67ae84ad1ef28f7f8d790c3ffc976747f168f43b
Terrence <terrencebattle30@gmail.com>
2025-01-27 10:24:34 -0500

    Second

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+    devServer: {
+        static: './dist',
+        port: 3000,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    mode: 'development',
+    devtool: 'source-map'
+};
